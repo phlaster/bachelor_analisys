4e7dc3048fca4c570c796379f28b1ffa6fc71e6f
Alex <username@users.noreply.github.com>
2025-03-24 22:22:06 +0300

    notebook with data loder preparations

--- notebooks/prepare_serialized.jl
+++ notebooks/prepare_serialized.jl
@@ -11,21 +11,24 @@ begin
 end
 
 # ╔═╡ 06ea3164-06e5-4f0f-8dc1-7b70ee3e45ca
-using FastaIO, Serialization
+using FastaIO, SimplePlutoInclude
 
-# ╔═╡ 453350bb-1bc7-4b41-816c-8a9c69313271
-include("../src/utils.jl")
+# ╔═╡ d2cef288-9178-47b8-945b-eca56ebe5cd2
+using Serialization, ProgressLogging
+
+# ╔═╡ ada341a4-9f03-476d-a70e-4268448c7e58
+@plutoinclude "../src/utils.jl"
 
 # ╔═╡ 246df942-64e6-4ce0-81f3-be42a2e6369f
 accs = readdir("../DATA/genomes/genomes")
 
 # ╔═╡ ab1a738a-206d-409a-b377-aa42dda21a7a
-function split_intervals_by_remainder(ranges::Vector{UnitRange{Int64}})
+function split_intervals_by_remainder(vec_of_ranges::Vector{UnitRange{Int}})
     rems0 = UnitRange{Int64}[]
     rems1 = UnitRange{Int64}[]
     rems2 = UnitRange{Int64}[]
 
-    for range in ranges
+    for range in vec_of_ranges
         first_elem = first(range)
         rem = mod(first_elem, 3)
         if rem == 0
@@ -41,19 +44,19 @@ function split_intervals_by_remainder(ranges::Vector{UnitRange{Int64}})
 end
 
 # ╔═╡ 5ba62567-4842-4763-a03a-0216e2c5d015
-function non_intersecting_intervals(ranges::Vector{UnitRange{Int64}})
+function anyintersect(ranges::Vector{UnitRange{Int}})
     n = length(ranges)
     for i in 1:(n-1)
         for j in (i+1):n
             low = max(first(ranges[i]), first(ranges[j]))
             high = min(last(ranges[i]), last(ranges[j]))
             if low <= high
-				println(ranges[i], "  ",ranges[j])
-                return false
+				@warn "Intersection between $i-th ($(ranges[i])) and $j-th ($(ranges[j])) intervals"
+                return true
             end
         end
     end
-    return true
+    return false
 end
 
 # ╔═╡ 8a8f5462-eff8-4d52-bd30-73b846bddd97
@@ -86,82 +89,159 @@ end
 
 
 # ╔═╡ 6d90398e-5544-4c89-b899-2f66ec6912e3
-function onehot_encode_dna(seq::AbstractString)
+function onehot_encode_dna(seq::AbstractString; revcomp=false)
     N = length(seq)
     encoding = zeros(UInt8, N, 4)
-    
-    mapping = Dict{Char,UInt8}('A'=>0x1, 'G'=>0x2, 'T'=>0x3, 'C'=>0x4)
-    
-    for (i, ch) in enumerate(seq)
-        if haskey(mapping, ch)
-            col = mapping[ch]
-            encoding[i, col] = 1
-        end
-    end
-    
+
+	if revcomp
+		comp_mapping = Dict{Char,UInt8}('A'=>0x1, 'C'=>0x2, 'G'=>0x3, 'T'=>0x4)
+	    for (i, ch) in enumerate(Iterators.reverse(uppercase(seq)))
+	        if haskey(comp_mapping, ch)
+	            col = comp_mapping[ch]
+	            encoding[i, col] = 0x1
+	        end
+	    end
+	else
+		mapping = Dict{Char,UInt8}('T'=>0x1, 'G'=>0x2, 'C'=>0x3, 'A'=>0x4)
+	    for (i, ch) in enumerate(uppercase(seq))
+	        if haskey(mapping, ch)
+	            col = mapping[ch]
+	            encoding[i, col] = 0x1
+	        end
+	    end
+	end
     return encoding
 end
 
+# ╔═╡ 389bddda-5acf-4f01-9da7-2bf717675f80
+function CDS_borders_in_GFF(gff_data, chromosomes::Vector{<:AbstractString}; strand="+")
+	@assert !isempty(gff_data) "Empty gff data"
+	@assert !isempty(chromosomes) "Empty chromosomes list"
+	@assert strand in ["+", "-"] "wrong strand identifier: $strand"
+	
+	cds_starts = Vector{UInt8}[]
+	cds_stops = Vector{UInt8}[]
+	for chrom in chromosomes
+		region_filter = filter_gff_region(; sequence_header=chrom, regiontype="region")
+
+		chrom_length = gff_data |> region_filter |> ranges_from_GFF_records |> only |> maximum
+
+		cds_filter = filter_gff_region(;
+			sequence_header=chrom,
+			regiontype="CDS",
+			strand=strand
+		)
+		intervals = ranges_from_GFF_records(cds_filter(gff_data))
+		
+
+		chrom_starts = zeros(UInt8, chrom_length)
+		chrom_stops = zeros(UInt8, chrom_length)
+
+		if isempty(intervals)
+			push!(cds_starts, chrom_starts)
+			push!(cds_stops, chrom_stops)
+			continue
+		end
+
+		starts = replace(
+			getfield.(intervals, :start) .% chrom_length,
+			0 => chrom_length
+		)
+		stops = replace(
+			getfield.(intervals, :stop) .% chrom_length,
+			0 => chrom_length
+		)
+		@assert length(starts) == length(stops) "start-stop count mismatch"
+		chrom_starts[starts] .= 0x1
+		chrom_stops[stops] .= 0x1
+		
+		push!(cds_starts, chrom_starts)
+		push!(cds_stops, chrom_stops)
+	end
+	
+	return strand == "+" ?
+		(cds_starts, cds_stops) :
+		(reverse!.(cds_starts), reverse!.(cds_stops))
+end
+
 # ╔═╡ 678cdb2c-b59b-41a2-b5d4-08be245859f0
-function assemble_tensor(genome_dir)
+function digitize_genome(genome_dir)
+	@assert isdir(genome_dir) "No such genomic directory"
 	file_prefix = last(splitpath(genome_dir))
 	fastaname = joinpath(genome_dir, "$file_prefix.fna")
 	gffname = joinpath(genome_dir, "$file_prefix.gff")
+	@assert all(isfile.([fastaname, gffname])) "Genomic files not found"
 	
 	fasta_data = readfasta(fastaname)
+	fasta_strings = getindex.(fasta_data, 2)
+	
+	dna_matrix_pos = onehot_encode_dna.(fasta_strings; revcomp=false)
+	dna_matrix_neg = onehot_encode_dna.(fasta_strings; revcomp=true)
+	
+	chromosomes = getindex.(fasta_data, 1) .|> split .|> first
 	gff_data = open_gff(gffname)
+	
+	starts_pos, ends_pos = CDS_borders_in_GFF(gff_data, chromosomes; strand="+")
+	starts_neg, ends_neg = CDS_borders_in_GFF(gff_data, chromosomes; strand="-")
 
-	full_dna_matrix = onehot_encode_dna(join(getindex.(fasta_data, 2)))
+	@assert all(==(length(fasta_strings)), [
+		length(dna_matrix_pos),
+		length(dna_matrix_neg),
+		length(starts_pos),
+		length(ends_pos),
+		length(starts_neg),
+		length(ends_neg)
+	]) "Dimentions do not match"
 
-	chrom_names = getindex.(fasta_data, 1) .|> split .|> first
-	@show length(chrom_names)
-	tensors = []
-	fasta_onehot = []
-	for ch in chrom_names
-		chrom_length = filter_gff_region(;
-			sequence_header=string(ch), regiontype="region"
-		)(gff_data) |> ranges_from_GFF_records |> only |> maximum
-		@show chrom_length
-		
-		chrom_tenz = zeros(UInt8, chrom_length, 2, 3, 3)
-		for (strand_dim, strand) in enumerate(["+", "-"])
-			extraction_filter = filter_gff_region(;
-				sequence_header=string(ch),
-				regiontype="CDS",
-				strand=strand
-			)
-			xtrcted_intrvls = ranges_from_GFF_records(extraction_filter(gff_data))
-			
-			frames_0_1_2 = xtrcted_intrvls |>
-				(x->filter(y->mod(length(y),3)==0, x)) |>
-				split_intervals_by_remainder
-
-			all_ranges_to_feature_tensor!(
-				chrom_tenz, frames_0_1_2, chrom_length, strand_dim
-			)		
-		end
-		push!(tensors, chrom_tenz)
-	end
-	full_acc_tensor = cat(tensors..., dims=1)
-	@show size(full_acc_tensor), size(full_dna_matrix)
-	return full_acc_tensor, full_dna_matrix
+	return dna_matrix_pos, dna_matrix_neg, starts_pos, ends_pos, starts_neg, ends_neg
 end
 
 # ╔═╡ 7dede4f3-0063-4578-9bf4-6e29474531c3
 rand_dir = "../DATA/genomes/genomes/$(rand(accs))"
 
-# ╔═╡ f2e3441a-f112-4c8a-8e71-de46504a09e2
-@time random_genome_encoded = assemble_tensor(rand_dir);
+# ╔═╡ 3eaf6a63-182d-40f1-b96e-78324d403b4b
+fixed_dir = "../DATA/genomes/genomes/$(accs[42])"
+
+# ╔═╡ 7e432565-a843-4703-827b-9f09eea70c1b
+# ╠═╡ disabled = true
+#=╠═╡
+@Threads.threads for (i, acc) in collect(enumerate(accs))
+	dir = "../DATA/genomes/genomes/$acc"
+	dg_name = "../DATA/genomes/serialized/$acc"
+	isfile(dg_name) && continue
+	try
+		digitize_genome(dir)
+		if mod(i, 100) == 0
+			println(i)
+		end
+		# serialize(dg_name, dg)
+	catch e
+		@warn i, dir
+		break
+	end
+end
+  ╠═╡ =#
+
+# ╔═╡ 085437f6-604d-40bc-bd34-7c7e024634e1
+single =  digitize_genome("../DATA/genomes/genomes/GCF_000010825.1")
+
+# ╔═╡ b2607609-9242-4609-9920-b334612cd191
+single[3]
 
 # ╔═╡ Cell order:
 # ╠═7f3c544c-0766-11f0-3da6-b3c5c6b653e3
 # ╠═06ea3164-06e5-4f0f-8dc1-7b70ee3e45ca
-# ╠═453350bb-1bc7-4b41-816c-8a9c69313271
+# ╠═ada341a4-9f03-476d-a70e-4268448c7e58
 # ╠═246df942-64e6-4ce0-81f3-be42a2e6369f
 # ╠═ab1a738a-206d-409a-b377-aa42dda21a7a
 # ╠═5ba62567-4842-4763-a03a-0216e2c5d015
 # ╠═8a8f5462-eff8-4d52-bd30-73b846bddd97
 # ╠═6d90398e-5544-4c89-b899-2f66ec6912e3
+# ╠═389bddda-5acf-4f01-9da7-2bf717675f80
 # ╠═678cdb2c-b59b-41a2-b5d4-08be245859f0
 # ╠═7dede4f3-0063-4578-9bf4-6e29474531c3
-# ╠═f2e3441a-f112-4c8a-8e71-de46504a09e2
+# ╠═3eaf6a63-182d-40f1-b96e-78324d403b4b
+# ╠═d2cef288-9178-47b8-945b-eca56ebe5cd2
+# ╠═7e432565-a843-4703-827b-9f09eea70c1b
+# ╠═085437f6-604d-40bc-bd34-7c7e024634e1
+# ╠═b2607609-9242-4609-9920-b334612cd191
