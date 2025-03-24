1699cfbd6248f7e3175a437e7bd1ffdc9a4f1216
phlaster <phlaster@users.noreply.github.com>
2025-03-24 21:24:58 +0000

    added data extracting from fasta and gff

--- src/utils.jl
+++ src/utils.jl
@@ -1,6 +1,7 @@
 using GFF3
 using DataFrames
 using PrettyTables
+using FastaIO
 
 function clear_last_lines(n::Int)
     for _ in 1:n
@@ -251,11 +252,11 @@ Create a filter function for GFF3 records based on multiple criteria:
 The returned function filters vectors of GFF3.Record objects.
 """
 filter_gff_region(;
-    sequence_header::T="",
-    regiontype::T="",
-    strand::T="",
+    sequence_header="",
+    regiontype="",
+    strand="",
     phase::Int=-1
-) where T <: AbstractString = list::Vector{GFF3.Record} -> filter(
+) = list::Vector{GFF3.Record} -> filter(
     x -> all([
         isempty(regiontype)      || GFF3.featuretype(x) == regiontype,
         isempty(sequence_header) || GFF3.seqid(x) == sequence_header,
@@ -489,4 +490,111 @@ function intersection_ratios(ref::Vector{UnitRange{Int}}, item::Vector{UnitRange
     end
     
     return result
+end
+
+
+function onehot_encode_dna(seq::AbstractString; revcomp=false)
+    N = length(seq)
+    encoding = zeros(UInt8, N, 4)
+
+    if revcomp
+        comp_mapping = Dict{Char,UInt8}('A'=>0x1, 'C'=>0x2, 'G'=>0x3, 'T'=>0x4)
+        for (i, ch) in enumerate(Iterators.reverse(uppercase(seq)))
+            if haskey(comp_mapping, ch)
+                col = comp_mapping[ch]
+                encoding[i, col] = 0x1
+            end
+        end
+    else
+        mapping = Dict{Char,UInt8}('T'=>0x1, 'G'=>0x2, 'C'=>0x3, 'A'=>0x4)
+        for (i, ch) in enumerate(uppercase(seq))
+            if haskey(mapping, ch)
+                col = mapping[ch]
+                encoding[i, col] = 0x1
+            end
+        end
+    end
+    return encoding
+end
+
+
+function CDS_borders_in_GFF(gff_data, chromosomes::Vector{<:AbstractString}; strand="+")
+    @assert !isempty(gff_data) "Empty gff data"
+    @assert !isempty(chromosomes) "Empty chromosomes list"
+    @assert strand in ["+", "-"] "wrong strand identifier: $strand"
+    
+    cds_starts = Vector{UInt8}[]
+    cds_stops = Vector{UInt8}[]
+    for chrom in chromosomes
+        region_filter = filter_gff_region(; sequence_header=chrom, regiontype="region")
+
+        chrom_length = gff_data |> region_filter |> ranges_from_GFF_records |> only |> maximum
+
+        cds_filter = filter_gff_region(;
+            sequence_header=chrom,
+            regiontype="CDS",
+            strand=strand
+        )
+        intervals = ranges_from_GFF_records(cds_filter(gff_data))
+        
+
+        chrom_starts = zeros(UInt8, chrom_length)
+        chrom_stops = zeros(UInt8, chrom_length)
+
+        if isempty(intervals)
+            push!(cds_starts, chrom_starts)
+            push!(cds_stops, chrom_stops)
+            continue
+        end
+
+        starts = replace(
+            getfield.(intervals, :start) .% chrom_length,
+            0 => chrom_length
+        )
+        stops = replace(
+            getfield.(intervals, :stop) .% chrom_length,
+            0 => chrom_length
+        )
+        @assert length(starts) == length(stops) "start-stop count mismatch"
+        chrom_starts[starts] .= 0x1
+        chrom_stops[stops] .= 0x1
+        
+        push!(cds_starts, chrom_starts)
+        push!(cds_stops, chrom_stops)
+    end
+    
+    return strand == "+" ?
+        (cds_starts, cds_stops) :
+        (reverse!.(cds_starts), reverse!.(cds_stops))
+end
+
+function digitize_genome(genome_dir::T) where T <: AbstractString
+    @assert isdir(genome_dir) "No such genomic directory"
+    file_prefix = last(splitpath(genome_dir))
+    fastaname = joinpath(genome_dir, "$file_prefix.fna")
+    gffname = joinpath(genome_dir, "$file_prefix.gff")
+    @assert all(isfile.([fastaname, gffname])) "Genomic files not found"
+    
+    fasta_data = readfasta(fastaname)
+    fasta_strings = getindex.(fasta_data, 2)
+    
+    dna_matrix_pos = onehot_encode_dna.(fasta_strings; revcomp=false)
+    dna_matrix_neg = onehot_encode_dna.(fasta_strings; revcomp=true)
+    
+    chromosomes = getindex.(fasta_data, 1) .|> split .|> first
+    gff_data = open_gff(gffname)
+    
+    starts_pos, ends_pos = CDS_borders_in_GFF(gff_data, chromosomes; strand="+")
+    starts_neg, ends_neg = CDS_borders_in_GFF(gff_data, chromosomes; strand="-")
+
+    @assert all(==(length(fasta_strings)), [
+        length(dna_matrix_pos),
+        length(dna_matrix_neg),
+        length(starts_pos),
+        length(ends_pos),
+        length(starts_neg),
+        length(ends_neg)
+    ]) "Dimentions do not match"
+
+    return dna_matrix_pos, dna_matrix_neg, starts_pos, ends_pos, starts_neg, ends_neg
 end
