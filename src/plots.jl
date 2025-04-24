874437e9d0189adff2d4559442fa9c1d59582f2b
phlaster <phlaster@users.noreply.github.com>
2025-04-24 11:16:37 +0000

    new plotting functions

--- src/plots.jl
+++ src/plots.jl
@@ -234,3 +234,237 @@ function summaryplots(main_df; train_df=nothing)
     )
     return fig
 end
+
+function plot_model_metrics(models::Vector)
+    marker_symbols = Dict(
+        :loss => "circle",
+        :lr   => "square",
+        :prec => "diamond",
+        :rec  => "cross",
+        :f1   => "triangle-up",
+        :fdr  => "triangle-down"
+    )
+
+    colors = ["red", "blue", "green", "orange", "purple", "brown", "pink", "gray"]
+
+    plt = PlotlyJS.make_subplots(
+        rows=2, cols=1,
+        shared_xaxes=true,
+        vertical_spacing=0.1,
+        specs=[
+            Spec() Spec()
+            Spec() Spec()
+        ]
+    )
+
+    for (i, m) in enumerate(models)
+        color = colors[mod1(i, length(colors))]
+        epochs = 1:length(m.lr)
+        model_name = split(m.dir, '/')[end][24:end]
+        trace_lr = scatter(
+            x=epochs, y=m.lr, mode="lines+markers",
+            name="$model_name lr", marker=attr(symbol=marker_symbols[:lr], color=color, size=10),
+            line=attr(color=color))
+        trace_loss = scatter(
+            x=epochs, y=m.loss, mode="lines+markers",
+            name="$model_name loss", marker=attr(symbol=marker_symbols[:loss], color=color, size=10),
+            line=attr(color=color, dash="dash"))
+        PlotlyJS.add_trace!(plt, trace_lr, row=1, col=1)
+        PlotlyJS.add_trace!(plt, trace_loss, row=1, col=1)
+
+
+        metrics_data = Dict((class, mn) => Float64[] for class in (0,1) for mn in (:prec, :rec, :f1, :fdr))
+        for data_point in m.metrics
+            for class in (1, ) # (0, 1)
+                nt = data_point[class]
+                for mn in (:prec, :rec, :f1, :fdr)
+                    push!(metrics_data[(class, mn)], getfield(nt, mn))
+                end
+            end
+        end
+
+        for class in (1, )# (0, 1)
+            for mn in (:prec, :rec, :f1, :fdr)
+                trace = scatter(
+                    x=epochs, y=metrics_data[(class, mn)], mode="lines+markers",
+                    name="$model_name class $(class) $(mn)",
+                    marker=attr(symbol=marker_symbols[mn], color=color, size=10),
+                    line=attr(color=color, dash=(class==0 ? "solid" : "dot")))
+                PlotlyJS.add_trace!(plt, trace, row=2, col=1)
+            end
+        end
+    end
+
+    PlotlyJS.relayout!(plt,
+        title="Model Metrics over Epochs",
+        xaxis=attr(title="Epochs"),
+        xaxis2=attr(title="Epochs"),
+        yaxis=attr(title="lr/loss (log scale)", type="log"),
+        yaxis2=attr(title="metrics")
+    )
+
+    return plt
+end
+
+_nbins_Rice(n) = ceil(Int, 2cbrt(n))
+
+function simple_hist(v::Vector)
+    L = length(v)
+    nbins_Rice = _nbins_Rice(L)
+    trace = histogram(x=v, nbinsx=nbins_Rice)
+
+    layout = Layout(
+        title="Histogram for $L values",
+        xaxis_title="Value",
+        yaxis_title="Count",
+        bargap=0.01,
+    )
+    
+    return Plot(trace, layout)
+end
+
+function plot_mismatch_histograms(gt_s2s_ranges::Dict{Int, Int}, pred_s2s_ranges::Dict{Int, Int}, fp_shifts::Dict{Int, Int})
+    gt_values = Iterators.flatten(Iterators.cycle(k, v) for (k,v) in gt_s2s_ranges) |> collect
+    pred_values = Iterators.flatten(Iterators.cycle(k, v) for (k,v) in pred_s2s_ranges) |> collect
+    fp_values = Iterators.flatten(Iterators.cycle(k, v) for (k,v) in fp_shifts) |> collect
+
+    gt_nbins = _nbins_Rice(length(gt_values))
+    pred_nbins = _nbins_Rice(length(pred_values))
+    fp_nbins = _nbins_Rice(length(fp_values))
+
+    gt_trace = histogram(
+        x=gt_values,
+        name="Ground Truth",
+        marker_color="#636EFA",
+        opacity=0.6,
+        nbinsx=gt_nbins
+    )
+
+    pred_trace = histogram(
+        x=pred_values,
+        name="Predicted",
+        marker_color="#EF553B",
+        opacity=0.6,
+        nbinsx=pred_nbins
+    )
+
+    fp_trace = histogram(
+        x=fp_values,
+        name="False Positive Shifts",
+        marker_color="#00CC96",
+        nbinsx=fp_nbins
+    )
+
+    min_val = 0
+    max_val = 1000
+
+    filtered_gt = filter(x -> min_val <= x <= max_val, gt_values)
+    filtered_pred = filter(x -> min_val <= x <= max_val, pred_values)
+    
+    gt_nbins_zoom = calculate_nbins(filtered_gt)
+    pred_nbins_zoom = calculate_nbins(filtered_pred)
+    common_nbins = max(gt_nbins_zoom, pred_nbins_zoom)
+    edges = range(min_val, max_val, length=common_nbins+1)
+    bin_width = edges[2] - edges[1]
+    
+    gt_counts = fit(Histogram, filtered_gt, edges).weights
+    pred_counts = fit(Histogram, filtered_pred, edges).weights
+    
+    offset = bin_width * 0.2
+    gt_x = edges[1:end-1] .+ bin_width/2 .- offset/2
+    pred_x = edges[1:end-1] .+ bin_width/2 .+ offset/2
+
+    gt_trace_zoom = bar(
+        x=gt_x,
+        y=gt_counts,
+        name="Ground Truth $min_val..$max_val",
+        width=bin_width*0.6,
+        marker_color="#636EFA",
+        offsetgroup="group1",
+        showlegend=false
+    )
+
+    pred_trace_zoom = bar(
+        x=pred_x,
+        y=pred_counts,
+        name="Predicted $min_val..$max_val",
+        width=bin_width*0.6,
+        marker_color="#EF553B",
+        offsetgroup="group1",
+        showlegend=false
+    )
+
+    total_fp = sum(values(fp_shifts))
+    shift_range = -30:30
+    percentages = [get(fp_shifts, s, 0)/total_fp * 100 for s in shift_range]
+    
+    tickvals = collect(-30:3:30)
+    ticktext = string.(tickvals)
+    
+    fp_bar_trace = bar(
+        x=collect(shift_range),
+        y=percentages,
+        name="FP Shift Distribution",
+        marker_color="#00CC96"
+    )
+
+    fig = make_subplots(
+        rows=2,
+        cols=2,
+        shared_xaxes=false,
+        vertical_spacing=0.1,
+        horizontal_spacing=0.1,
+        subplot_titles=[
+        "Start-to-Start Length Distribution" "Start-to-Start $min_val..$max_val";
+        "False Positive Shifts"              "Shift Distribution $shift_range"
+        ]
+    )
+
+    add_trace!(fig, gt_trace, row=1, col=1)
+    add_trace!(fig, pred_trace, row=1, col=1)
+    add_trace!(fig, fp_trace, row=2, col=1)
+    add_trace!(fig, gt_trace_zoom, row=1, col=2)
+    add_trace!(fig, pred_trace_zoom, row=1, col=2)
+    add_trace!(fig, fp_bar_trace, row=2, col=2)
+
+    relayout!(
+        fig,
+        barmode="overlay",
+        title_text="Sequence Analysis Histograms",
+
+        xaxis=attr(title="Length", showgrid=false),
+        yaxis=attr(type="log", title="Count (log scale)", showgrid=true),
+        xaxis3=attr(title="Shift Value", showgrid=false),
+        yaxis3=attr(type="log", title="Count (log scale)", showgrid=true),
+
+        xaxis2=attr(
+            title="Length $min_val..$max_val",
+            showgrid=false,
+            range=[-bin_width, max_val+bin_width]
+        ),
+        yaxis2=attr(
+            type="log",
+            title="Count (log scale)",
+            showgrid=true
+        ),
+        xaxis4=attr(
+            title="Shift Value $shift_range",
+            showgrid=false,
+            tickmode="array",
+            tickvals=tickvals,
+            ticktext=ticktext,
+            tickfont=attr(size=8),
+            range=[shift_range.start-0.5, shift_range.stop+0.5]
+        ),
+        yaxis4=attr(
+            type="linear",
+            title="Percentage of Total (%)",
+            showgrid=true
+        ),
+
+        legend=attr(x=1.05, y=1, orientation="v"),
+        margin=attr(l=50, r=50, t=80, b=50)
+    )
+
+    return fig
+end
